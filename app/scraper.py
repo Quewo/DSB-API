c49c1a50ad2c3ce73c6955bac5ac8fb8aaa7160e
Quewo <fpm.jacob@gmail.com>
2024-06-24 23:00:16 +0200

    Added load config

--- app/scraper.py
+++ app/scraper.py
@@ -1,13 +1,25 @@
 import json
 import bs4
 import requests
+import os
+
+
+# load the config.json file and set global variables
+def load_config() -> None:
+     # Get the directory of the current file (scraper.py)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # Construct the path to config.json
+    config_path = os.path.join(dir_path, 'config.json')
+
+    with open(config_path) as config_file:
+        global config_data, URL
+        config_data = json.load(config_file)
+        URL = config_data['url']
+
 
 # Read the URL from config.json
 def scrape():
-    with open('config.json') as config_file:
-        global config_data
-        config_data = json.load(config_file)
-        URL = config_data['url']
+    load_config()
 
     req = requests.get(URL)
     if req.status_code == 200:
